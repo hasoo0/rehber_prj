0d0db303e139195a3b3b7ac159786936fd9a36c9
hasan huseyin <hkizilay65@gmail.com>
2021-12-11 13:25:51 +0300

    Initial commit

---
+++ src/com/bilgeadam/rehberprj/vt/VTBaglanti.java
@@ -0,0 +1,24 @@
+package com.bilgeadam.rehberprj.vt;
+
+import java.sql.*;
+
+public class VTBaglanti {
+
+    public static Connection baglantiGetir()
+            throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+
+
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/rehber", "postgres", "123456");
+
+
+
+        return conn;
+    }
+
+    public static  void baglantiKapat(Connection conn) throws SQLException {
+
+        conn.close();
+    }
+
+}
