ca4596164f6f12f6495cc5d001383860fd09c325
hasan huseyin <hkizilay65@gmail.com>
2021-12-18 14:01:06 +0300

    tarih cevirme islemleri eklendi

--- src/com/bilgeadam/rehberprj/util/Sabitler.java
+++ src/com/bilgeadam/rehberprj/util/Sabitler.java
@@ -5,5 +5,5 @@ public class Sabitler {
     public static final  String SECRET_KEY = "gizli_bahce";
     public static final  String TARIH_FORMAT = "dd/MM/yyyy";
     public static final  String SAAT_FORMAT = "HH:mm:ss";
-    public static final  String TARIH_SAAT_FORMAT = TARIH_FORMAT;
+    public static final  String TARIH_SAAT_FORMAT = TARIH_FORMAT + " " + SAAT_FORMAT;
 }
